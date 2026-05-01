6dae85ab7253a2ff61605a44584b14ba6964e256
EthanCharpentier667 <ethan.charpentier@epitech.eu>
2026-05-01 20:51:49 +0200

    feat (render): add a fixed onfiguration in Core to test render

--- src/Core.cpp
+++ src/Core.cpp
@@ -9,11 +9,28 @@
 
 #include "plugin/PluginManager.hpp"
 
+#include "math/Color.hpp"
+
+#include "object/ALight.hpp"
+#include "object/AScene.hpp"
+#include "object/AObject.hpp"
+
 namespace raytracer {
 
     void Core::init() {
         this->_plugManager.updatePluginList(PLUGINS_FOLDER_PATH);
         this->_plugManager.fillFactory(this->_objFactory);
+
+        //EXMPLE SCNE WITHOUT CONFIGURATION FILE
+
+        std::map<std::string, std::any> sceneParams;
+        sceneParams["aoSamples"] = 16; // Example: 16 samples for ambient occlusion 0 for no ambient occlusion
+        sceneParams["aoRadius"] = 1.0; // Example: radius of 1.0 for ambient occlusion effect, 0 for no ambient occlusion
+        sceneParams["ambientLightColor"] = maths::Color(255, 255, 255); // Example: white ambient light, 0 for no ambient light
+        sceneParams["ambientLightIntensity"] = 0.1; // Example: low intensity for ambient light, 0 for no ambient light
+        sceneParams["ambientDiffuseColor"] = maths::Color(255, 255, 255); // Example: white ambient diffuse light, 0 for no ambient diffuse light
+        sceneParams["ambientDiffuseIntensity"] = 0.2; // Example: low intensity for ambient diffuse light, 0 for no ambient diffuse light
+        auto scene = std::make_shared<object::scene::IScene>(sceneParams);
     }
 
     void Core::run() {
