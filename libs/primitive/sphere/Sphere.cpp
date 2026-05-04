8eb4272ad995b082286cf49f54b1da1bac8bd53c
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-05-04 16:23:51 +0200

    style(Sphere.cpp): add somes minor changes for remove linter warning

--- libs/primitive/sphere/Sphere.cpp
+++ libs/primitive/sphere/Sphere.cpp
@@ -19,14 +19,14 @@
 #include "util/middleware/ObjectMiddleware.hpp"
 
 namespace raytracer::object::primitive {
-    Sphere::Sphere(const std::map<std::string, std::any> &params)
+    Sphere::Sphere(const std::map<std::string, std::any> &args)
         : APrimitive(
-              "Sphere", util::Helpers::toVector(params, "center", "Sphere"),
+              "Sphere", util::Helpers::toVector(args, "center", "Sphere"),
               util::ObjectMiddleware::validate<
                   std::shared_ptr<raytracer::object::material::IMaterial>>(
-                  params, "material", "Sphere")),
+                  args, "material", "Sphere")),
           _radius(
-              util::ObjectMiddleware::validate<double>(params, "r", "Sphere")) {
+              util::ObjectMiddleware::validate<double>(args, "r", "Sphere")) {
         util::Helpers::unsignedDouble(_radius, "r", "Sphere");
     }
 
@@ -79,11 +79,9 @@ namespace raytracer::object::primitive {
     }
 
     SurfaceData Sphere::surfaceData(const maths::Vector &hitPoint) const {
-        maths::Vector normal = (hitPoint - _center).normalized();
-
-        double u = 0.5 + std::atan2(normal.z, normal.x) / (2 * M_PI);
-        double v = 0.5 - std::asin(normal.y) / M_PI;
-
+        const maths::Vector normal = (hitPoint - _center).normalized();
+        const double u = 0.5 + std::atan2(normal.z, normal.x) / (2 * M_PI);
+        const double v = 0.5 - std::asin(normal.y) / M_PI;
         SurfaceData data{
             .normal = normal, .uv = maths::Vector(u, v, 0), .material = {}};
 
