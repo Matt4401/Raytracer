21d340e47c03c7e1945c41b037cd73b9fee0da76
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-05-08 18:26:04 +0200

    doc(BoundingBox): add docxygen for bounding box class methods

--- include/math/BoundingBox.hpp
+++ include/math/BoundingBox.hpp
@@ -25,8 +25,32 @@ namespace raytracer::maths {
 
         BoundingBox();
 
+        /// @brief Extend the current bounding box to include another bounding
+        /// box. This function modifies the current bounding box so that it
+        /// encompasses the volume defined by both the current bounding box and
+        /// the other bounding box.
+        /// @param other The bounding box to extend the current bounding box to
+        /// include. This should be a valid BoundingBox object with defined
+        /// position and dimensions.
+        /// @return void
         void extend(const BoundingBox &other);
+
+        /// @brief Calculate the surface area of the bounding box. The surface
+        /// area is calculated using the formula: surface area = 2 * (w * h + w
+        /// * d + h * d), where w, h, and d are the width, height, and depth of
+        /// the bounding box, respectively.
+        /// @return the surface area of the bounding box, or 0 if any dimension
+        /// is negative
         double surfaceData() const;
+
+        /// @brief Check if a ray intersects with the bounding box. This
+        /// function uses the "slab method" to determine if the ray intersects
+        /// with the bounding box.
+        /// @param ray The ray to test for intersection with the bounding box.
+        /// This should be a valid Ray object with defined origin and direction
+        /// components.
+        /// @return true if the ray intersects with the bounding box, false
+        /// otherwise
         bool intersects(const Ray &ray) const;
     };
 
