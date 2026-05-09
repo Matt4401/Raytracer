6354548e8cd6cf169004f9660c5654e2b8b065e1
TomGatin <tom.gatin@epitech.eu>
2026-05-09 18:24:36 +0200

    refactor(Noise): remove unused vector include and private member

--- include/math/Noise.hpp
+++ include/math/Noise.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <cmath>
-#include <vector>
 
 namespace raytracer::maths {
     class Noise {
@@ -20,8 +19,5 @@ namespace raytracer::maths {
         static double lerp(double a, double b, double t);
         static double grad(int hash, double x, double y, double z);
         static double perlin(double x, double y, double z);
-
-      private:
-        std::vector<int> _permutation;
     };
 }  // namespace raytracer::maths
