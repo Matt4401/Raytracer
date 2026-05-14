9c9a0b67b81215e1e5e97f2ebf1defbbe1a0d086
TomGatin <tom.gatin@epitech.eu>
2026-05-14 19:05:40 +0200

    feat(shear): add Shear primitive decorator and plugin implementation

---
+++ libs/transformations/shear/Shear.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** Raytracer
+** File description:
+** Shear
+*/
+
+#pragma once
+
+#include <any>
+#include <map>
+#include "object/transformations/ATransformDecorator.hpp"
+
+namespace raytracer::object::primitive {
+    class Shear : public ATransformDecorator {
+      public:
+        explicit Shear(const std::map<std::string, std::any> &params);
+        ~Shear() override = default;
+    };
+}  // namespace raytracer::object::primitive
