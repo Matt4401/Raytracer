d34811622f5d2f1457970666ab53c561f0a30c32
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-05-14 14:14:04 +0200

    feat(Cone & cylinder): add cone and cylinder plugin file with infinity gestion

---
+++ libs/primitive/cylinder/CylinderPlugin.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** CylinderPlugin
+*/
+
+#include <any>
+#include <map>
+#include <string>
+
+#include "Cylinder.hpp"
+#include "object/IObject.hpp"
+
+extern "C" raytracer::object::AObject::Type type(void) {
+    return raytracer::object::AObject::Type::PRIMITIVE;
+}
+
+extern "C" const char *name(void) {
+    return "cylinder";
+}
+
+extern "C" raytracer::object::BuilderFunc builder(void) {
+    return [](const std::map<std::string, std::any> &params)
+               -> raytracer::object::IObject * {
+        // height == limit cylinder
+        if (params.contains("height")) {
+            return new raytracer::object::primitive::LimitedCylinder(params);
+        }
+        return new raytracer::object::primitive::Cylinder(params);
+    };
+}
